02931f9bb23f4054a9a84ce3c9c7b4e3786e4af7
Niels Mateijsen <niels.mateijsen@hotmail.com>
2022-09-29 19:36:05 +0200

    Added DataGetter

---
+++ DataGetter.py
@@ -0,0 +1,24 @@
+import os
+
+from DBConnection import DBConnection
+import yaml
+
+
+class DataGetter:
+    graph_query = ""
+
+    os.chdir(os.path.dirname(__file__))
+    with open('sql.yaml') as f:
+        try:
+            graph_query = yaml.safe_load(f)["sql"]["graph"]
+        except yaml.YAMLError as e:
+            print(e)
+
+    def get_data(self, is_guest):
+        conn = DBConnection()
+        if self.graph_query != "":
+            data = conn.query(self.graph_query.format(str(is_guest)))
+            conn.close()
+            return data
+        conn.close()
+        raise Exception("\nEXCEPTION:\ngraph_query is empty")
---
+++ test/data_getter_test.py
@@ -0,0 +1,29 @@
+import os
+import unittest
+
+import yaml
+
+from DataGetter import DataGetter
+
+
+class MyTestCase(unittest.TestCase):
+    @staticmethod
+    def test_query_string_not_empty():
+        assert DataGetter.graph_query != ""
+
+    @staticmethod
+    def test_yaml_test_string():
+        os.chdir(os.path.dirname(__file__))
+        with open('../sql.yaml') as f:
+            test_string = yaml.safe_load(f)["test"]
+        assert test_string == "test"
+
+    @staticmethod
+    def test_get_data_not_none():
+        getter = DataGetter()
+        data = getter.get_data(False)
+        assert len(data) > 0
+
+
+if __name__ == '__main__':
+    unittest.main()
