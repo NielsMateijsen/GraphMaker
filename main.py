a27a3160f0ac8c03eebb7748447b413f188645cc
Niels Mateijsen <niels.mateijsen@hotmail.com>
2022-11-06 21:57:41 +0100

    Run every 5 min

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import graph_creator
+import time
 
 if __name__ == '__main__':
-    graph_creator.create_all_graphs()
+    while True:
+        graph_creator.create_all_graphs()
+        time.sleep(5*60)
