1dda9fd006ec5c52dbd8eb6f7d634a340de7b471
Niels Mateijsen <niels.mateijsen@hotmail.com>
2022-11-09 21:38:18 +0100

    Graph legend horizontal on top

--- graph_creator.py
+++ graph_creator.py
@@ -3,8 +3,8 @@ import plotly.express as px
 import data_getter
 
 image_path = "images/"
-image_width = 1200
-image_height = 700
+image_width = 1000
+image_height = 800
 
 
 def create_line_graph(is_guest):
@@ -12,7 +12,13 @@ def create_line_graph(is_guest):
     fig = px.line(df, markers=True, width=image_width, height=image_height)
     fig.update_yaxes(title=None)
     fig.update_xaxes(title=None)
-    fig.update_layout(legend_title=None)
+    fig.update_layout(legend_title=None, legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ))
     filename = "line_graph"
     if is_guest:
         filename += "_guest"
@@ -25,7 +31,13 @@ def create_bar_plot(is_guest):
     fig = px.bar(df, width=image_width, height=image_height, text_auto=True)
     fig.update_yaxes(title=None)
     fig.update_xaxes(title=None)
-    fig.update_layout(legend_title=None)
+    fig.update_layout(legend_title=None, legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ))
     filename = "bar_plot"
     if is_guest:
         filename += "_guest"
